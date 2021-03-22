fcd1b2c7be97a6509684d12c67c3b597796df5ce
LeshA5 <79832387+LeshA5@users.noreply.github.com>
2021-03-22 13:44:58 +0300

    Create task2.pas

---
+++ lw2/task2.pas
@@ -0,0 +1,23 @@
+PROGRAM SarahRevere(INPUT, OUTPUT);
+USES
+  DOS;
+VAR
+  QueryString: STRING;
+BEGIN
+  WRITELN('Content-Type: text/plain');
+  WRITELN;
+  QueryString := GetEnv('QUERY_STRING');
+  IF QueryString = 'lanterns=1'
+  THEN
+    WRITELN('The British are coming by land')
+  ELSE
+    IF QueryString = 'lanterns=2'
+    THEN
+      WRITELN('The British are coming by sea')
+    ELSE
+      IF QueryString = ''
+      THEN
+        WRITELN('The North church shows only NONE')
+      ELSE
+        WRITELN('The North church shows only ', GetEnv('QUERY_STRING'))
+END.
