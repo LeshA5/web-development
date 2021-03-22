cb08e586f5fbb4ac56399c74cb1719ba7c1af474
LeshA5 <79832387+LeshA5@users.noreply.github.com>
2021-03-22 13:50:26 +0300

    Update task2.pas

--- lw2/task2.pas
+++ lw2/task2.pas
@@ -7,17 +7,17 @@ BEGIN
   WRITELN('Content-Type: text/plain');
   WRITELN;
   QueryString := GetEnv('QUERY_STRING');
-  IF QueryString = 'lanterns=1'
+  IF QueryString = ''
   THEN
-    WRITELN('The British are coming by land')
+    WRITELN('The North church shows only NONE')
   ELSE
     IF QueryString = 'lanterns=2'
     THEN
       WRITELN('The British are coming by sea')
     ELSE
-      IF QueryString = ''
+      IF QueryString = 'lanterns=1'
       THEN
-        WRITELN('The North church shows only NONE')
+        WRITELN('The British are coming by land')
       ELSE
         WRITELN('The North church shows only ', GetEnv('QUERY_STRING'))
 END.
